c7c446122dcb1b43c84d343ed51d4a8bdfeed910
Fabrizio112 <fabri.avila3@gmail.com>
2023-03-22 11:11:13 -0300

    Termina programa y divide Conversion y Comparador de Divisas

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-var myHeaders = new Headers();
-myHeaders.append("apikey", "KXsiCwvBF9yxUugeOtslsAwgjIPmhdcX");
 
-var requestOptions = {
+const options = {
     method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-};
-
-let $botonBuscar = document.querySelector("#buscar")
-let $contenedorDeInputs = document.querySelector(".contenedor-programa-inicial")
-$botonBuscar.onclick = function () {
-    let valorDelInput = (document.querySelector("#input-divisa").value).toUpperCase();
-    if (contadorContenedor >= 1) {
-        eliminarEltituloDeLasDivisas();
-        eliminarElContenedorConLasDivisas();
+    headers: {
+        'X-RapidAPI-Key': '1cdc29d1fbmsh8c7f4b7d7ca48d8p1e78ddjsn2f86475e8467',
+        'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
     }
-    traerValoresEnComparacion(valorDelInput);
-    return false;
-}
+};
+let $botonDeCalcular = document.querySelector("#calcular")
+let $botonDeComparar = document.querySelector("#comparar");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// COMPARADOR DE DIVISAS     ////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$botonDeComparar.addEventListener('click', () => {
+    $("#container").append($(`<div class="contenedor-programa-comparar">
+    <h2 class="fs-1 text-uppercase p-3">Comparador de Divisas de Fabrizio</h2>
+    <label class="form-label mb-3 fs-4" for="">Ingrese la divisa :</label>
+    <input class="form-control mb-4 text-center fs-4" type="text" id="input-divisa">
+    <button class="btn btn-outline-primary px-5 fs-3" id="buscar">Buscar</button>
+</div>`))
+    document.querySelector("#buscar").addEventListener('click', () => {
+        let valorDelInput = (document.querySelector("#input-divisa").value).toUpperCase();
+        if (contadorContenedor >= 1) {
+            eliminarEltituloDeLasDivisas();
+            eliminarElContenedorConLasDivisas();
+        }
+        traerValoresEnComparacion(valorDelInput);
+        return false;
+    })
+    return false
+})
+
 function traerValoresEnComparacion(a) {
-    fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols=GBP%2CARS%2CUSD%2CCHF%2CJPY%2CHKD%2CCAD%2CCNY%2CAUD%2CBRL%2CRUB%2CMXN&base=${a}`, requestOptions)
+    fetch(`https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=${a}$&to=EUR%2CGBP%2CARS%2CUSD%2CCHF%2CJPY%2CHKD%2CCAD%2CMXN%2CCNY%2CAUD%2CBRL%2CRUB`, options)
         .then(respuesta => respuesta.json())
         .then(datos => {
             crearContenedoresDeDatos(datos)
@@ -56,3 +69,95 @@ function eliminarEltituloDeLasDivisas() {
 function eliminarElContenedorConLasDivisas() {
     document.querySelector("#contenedor-de-divs").remove();
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///// CALCULADOR DE DIVISAS //////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$botonDeCalcular.addEventListener('click', () => {
+    $(".container").append($(`
+    <div class="contenedor-programa-calcular">
+    <h3>Conversor de Monedas </h3>
+    <div class="contenedor-de-comparaciones">
+        <div class="contenedor-cantidad-a-convertir">
+            <label class="form-label" for="">Cantidad</label>
+            <div class="cantidad-a-convertir">
+                <input class="form-control" type="number" name="" id="input-convertir">
+                <select class="form-select" name="" id="select-a-convertir">
+                </select>
+            </div>
+        </div>
+        <div class="contenedor-cantidad-convertida">
+            <label class="form-label" for="">Convertido a </label>
+            <div class="cantidad-convertida">
+                <input class="form-control " type="text" name="" id="input-convertido" disabled>
+                <select class="form-select" name="" id="select-convertido">
+                </select>
+            </div>
+        </div>
+    </div>
+    <button class="btn btn-outline-primary px-5 fs-3" id="conversion">Hacer Conversion</button>
+</div>`
+    ))
+    traerValores();
+    let $select = document.querySelector("#select-a-convertir")
+    let $selectConvertido = document.querySelector("#select-convertido")
+    let $input = document.querySelector("#input-convertir")
+    let $inputConvertido = document.querySelector("#input-convertido")
+    let opcionSeleccionadaDelSelectAConvertir = "";
+    let opcionSeleccionadaDelSelectConvertido = "";
+    let valorEnTiempoRealDelInput = 0;
+    let $botonConversion = document.querySelector("#conversion")
+    $select.addEventListener('change',
+        function () {
+            opcionSeleccionadaDelSelectAConvertir = this.options[$select.selectedIndex].text;
+            console.log(opcionSeleccionadaDelSelectAConvertir)
+        });
+    $selectConvertido.addEventListener('change',
+        function () {
+            opcionSeleccionadaDelSelectConvertido = this.options[$selectConvertido.selectedIndex].text;
+            console.log(opcionSeleccionadaDelSelectConvertido)
+        });
+    $input.addEventListener('keyup',
+        function () {
+            valorEnTiempoRealDelInput = $(this).val();
+            console.log(valorEnTiempoRealDelInput)
+        });
+
+    $botonConversion.addEventListener("click", () => {
+        if (opcionSeleccionadaDelSelectAConvertir && opcionSeleccionadaDelSelectConvertido) {
+            traerValoresParaElConversorDeMonedas(opcionSeleccionadaDelSelectAConvertir, opcionSeleccionadaDelSelectConvertido, parseInt(valorEnTiempoRealDelInput), $inputConvertido)
+        } else {
+
+        }
+    })
+})
+
+
+
+function traerValoresParaElConversorDeMonedas(a, b, c, d) {
+    fetch(`https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=${a}&to=${b}&amount=${c}`, options)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            d.value = datos.result
+        })
+        .catch(error => console.log('error', error));
+}
+
+
+/* Hay una opcion de la api que permite ingresar la moneda para convertir a determinada moneda y el valor */
+/* Mas tarde voy a crearme otra cuenta para poder tener una api xd */
+
+function traerValores() {
+    fetch(`https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=USD&to=EUR%2CGBP%2CARS%2CUSD%2CCHF%2CJPY%2CHKD%2CCAD%2CMXN%2CCNY%2CAUD%2CBRL%2CRUB`, options)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            $("#select-a-convertir").append($(`<option value="">${datos.base}</option>`))
+            $("#select-convertido").append($(`<option value="">${datos.base}</option>`))
+            Object.keys(datos.rates).forEach((valor) => {
+                $("#select-a-convertir").append($(`<option value="">${valor}</option>`))
+                $("#select-convertido").append($(`<option value="">${valor}</option>`))
+            })
+        })
+        .catch(error => console.log('error', error));
+}
+
+
